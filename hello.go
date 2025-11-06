5cd0f73797a163040740522be5505737aa66909b
Jemimah Nagasha <jemynagasha@gmail.com>
2025-11-06 11:15:23 +0300

    refactor: handle more variables cleanly

--- hello.go
+++ hello.go
@@ -2,10 +2,28 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const spanish = "Spanish"
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == spanish {
+		return  spanishHelloPrefix + name
+	}
+	if language == french {
+		return  frenchHelloPrefix + name
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"));
+	name := ""
+	language := ""
+	fmt.Println(Hello(name, language));
 }
--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,31 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Jemimah")
-	want := "Hello, Jemimah"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Jemimah", "")
+		want := "Hello, Jemimah"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T){
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jemimah", "French")
+		want := "Bonjour, Jemimah"
+		assertCorrectMessage(t, got, want)
+	})
+	
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
